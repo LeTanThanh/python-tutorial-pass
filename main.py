5f62826a26e05e72f9bbd0d122e8a935c5bc9e03
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-27 00:19:56 +0700

    Introduction to the Python pass statement

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
-  print("Python pass")
+  # Introduction to the Python pass statement
+
+  cur_index = 1
+  max_index = 10
+  if cur_index <= max_index:
+    cur_index += 1
+  else:
+    # implement later
+    pass
